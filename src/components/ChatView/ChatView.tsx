83433a75ba092016c0d3befda65d25b41dc3fb82
handschrift <cahit.kilinc@gmx.de>
2022-11-26 14:39:08 +0100

    Made chat scroll automatically to bottom

--- src/components/ChatView/ChatView.tsx
+++ src/components/ChatView/ChatView.tsx
@@ -1,12 +1,23 @@
 import "./ChatView.css"
 import {ChatMessage} from "../../types/ChatMessage";
 import mika from "../../assets/mika.png"
+import {useEffect, useRef} from "react";
 
 type ChatViewProps = {
     history: ChatMessage[]
 }
 
 export default function ChatView(props: ChatViewProps) {
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({behavior: 'smooth'})
+    }
+
+    useEffect(() => {
+        scrollToBottom();
+    })
+
     return (
         <ul className="chatview-history">
             {props.history.map((element: ChatMessage, index: number) =>
@@ -15,6 +26,9 @@ export default function ChatView(props: ChatViewProps) {
                     <span className="chatview-chatbubble">{element.fromBot ?
                         <img src={mika} width={25} height={25} alt="Mika-Bot"/> : ""}{element.text} </span></li>
             )}
+            <li>
+                <div ref={messagesEndRef}></div>
+            </li>
         </ul>
     )
 }
